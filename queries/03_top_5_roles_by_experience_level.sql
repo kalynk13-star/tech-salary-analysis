76c1bc6707b211b7a5b73319c9bc60361fd1a19d
kalynk13-star <kalynking13@gmail.com>
2026-03-06 08:52:38 -0800

    Update 03_top_5_roles_by_experience_level.sql
    relabeled experience levels and with updated minimum threshold of 10

--- queries/03_top_5_roles_by_experience_level.sql
+++ queries/03_top_5_roles_by_experience_level.sql
@@ -2,7 +2,12 @@
 --- Filtered to full-time only
 WITH ranked_roles AS (
   SELECT
-    experience_level,
+    CASE experience_level
+      WHEN 'EN' THEN '1 - Entry'
+      WHEN 'MI' THEN '2 - Mid'
+      WHEN 'SE' THEN '3 - Senior'
+      WHEN 'EX' THEN '4 - Executive'
+    END AS experience_level,
     job_title,
     ROUND(AVG(salary_in_usd), 0) AS avg_salary_usd,
     COUNT (*) AS number_of_roles
@@ -10,6 +15,7 @@ WITH ranked_roles AS (
   WHERE employee_residence = 'US'
   AND employment_type = 'FT'
   GROUP BY experience_level, job_title
+  HAVING COUNT(*)>=10
   ),
   ranked AS (
     SELECT*,
@@ -23,6 +29,6 @@ WITH ranked_roles AS (
     number_of_roles,
     rank
   FROM ranked
-  WHERE rank<=5
+  WHERE rank<=3
   ORDER BY experience_level, rank
   LIMIT 40
